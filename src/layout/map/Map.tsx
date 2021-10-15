01bc77ab19f2361e38a8c6f38857e8698435cfef
dataenggu <dsolkim1030@gmail.com>
2021-10-15 15:09:10 +0900

    add onclickEvent

--- src/layout/map/Map.tsx
+++ src/layout/map/Map.tsx
@@ -1,19 +1,41 @@
-import { WorldMap } from "react-svg-worldmap"
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import './Map.scss'
+import React, { ReactElement, useEffect, useState } from "react";
+import { WorldMap } from "react-svg-worldmap";
+import "./Map.scss";
+import { CountryData } from "./model/CountryData.model";
 
-const countryData =
-    [
-        { country: "cn", value: 1389618778 }, // china
-        { country: "in", value: 1311559204 }, // india
-    ]
+const countryData = [
+  { country: "cn", value: 1 },
+  { country: "in", value: 3 },
+  { country: "kr", value: 2 },
+  { country: "us", value: 10 },
+]; // TODO : 추후 이 데이터는 DB에서 가져오도록.
 
 const Map = () => {
-    return (
-        <div className='MapWrapper'>
-            <WorldMap color="blue" size="xxl" data={countryData} />
-        </div>
-    )
-}
+  const handleOnclick = (
+    countryName: string,
+    countryCode: string,
+    countryValue: string
+  ) => {
+    console.log("선택 국가 : " + countryCode);
+    // TODO : 선택 국가 모달 띄우기
+    return {};
+  };
+
+  return (
+    <div className="MapWrapper">
+      <WorldMap
+        color="green"
+        size="xxl"
+        data={countryData}
+        onClickFunction={(
+          event: React.MouseEvent<SVGElement, Event>,
+          countryName: string,
+          isoCode: string,
+          value: string
+        ) => handleOnclick(countryName, isoCode, value)}
+      />
+    </div>
+  );
+};
 
 export default Map;
---
+++ src/layout/map/model/CountryData.model.ts
@@ -0,0 +1,5 @@
+export interface CountryData {
+  countryName: string;
+  countryCode: string;
+  countryValue: string;
+}
